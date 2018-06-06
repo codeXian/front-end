3310fe1b451c45d730b5ca44c6989a127d886ba2
codexian <codexian@yeah.net>
2018-06-06 23:15:07 +0800

    添加时间定时器

--- react/app-ts/src/App.tsx
+++ react/app-ts/src/App.tsx
@@ -1,19 +1,28 @@
 import * as React from 'react';
 import './App.css';
 
-import logo from './logo.svg';
-
 class App extends React.Component {
+  public state = {
+    time: new Date().toLocaleTimeString(),
+  };
+  public componentDidMount() {
+    setInterval(() => {
+      this.setTimeInterval();
+    }, 1000);
+  }
+
+  public setTimeInterval() {
+    this.setState({ time: new Date().toLocaleTimeString() });
+  }
+
   public render() {
+    const { time } = this.state;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <div>
+          <h1>Hello World</h1>
+          <h2>It is {time}</h2>
+        </div>
       </div>
     );
   }
